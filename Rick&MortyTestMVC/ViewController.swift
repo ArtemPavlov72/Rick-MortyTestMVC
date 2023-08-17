e7480baf158f9ceecaf646f3def89135bac0ea9b
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-17 14:08:38 +0500

    Initial Commit

---
+++ Rick&MortyTestMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&MortyTestMVC
+//
+//  Created by Артем Павлов on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
