eeac42e1d3a3f8a1966a9c2c5a7402406b7af8b6
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-17 14:24:05 +0500

    Initial commit. Add launch image.

---
+++ Rick&MortyTestMVC/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Rick&MortyTestMVC
+//
+//  Created by Artem Pavlov on 17.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
